42486b43ad3a24a7029bd76d6b2ea2f606a92aed
Aman Gupta <amangupta96@outlook.com>
2016-09-23 18:24:16 +0800

    Package implementation

---
+++ packages/index/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'index',
+  version: '0.0.1',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse((api)=> {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('templating'); 
+  api.use('iron:router');
+  api.use('tap:i18n@1.8.0');
+  api.use('reactive-var','client');
+}
+//For images
+//api.addAssets([]);
+
+
+// For files
+//   api.addFiles(['index.html','index.js'], 'client');
