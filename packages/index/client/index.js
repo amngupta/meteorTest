846c40eebe59dff887e2c19ac96683ed1bcb7feb
Apoorva Katta <apoorva@passkit.com>
2016-10-02 10:53:09 +0800

    added manual trade

--- packages/index/client/index.js
+++ packages/index/client/index.js
@@ -7,6 +7,12 @@ s0005 = new ReactiveVar();
 s0386 = new ReactiveVar();
 s0388 = new ReactiveVar();
 s3988 = new ReactiveVar();
+getExchange = new ReactiveVar();
+getStock = new ReactiveVar();
+getSide = new ReactiveVar();
+getOrderType = new ReactiveVar();
+getQuantity = new ReactiveVar();
+getPrice = new ReactiveVar();
 
 Template.hello.onCreated(function () {
   getTeamData();
@@ -15,33 +21,45 @@ Template.hello.onCreated(function () {
 Template.hello.helpers({
   cashBal() {
     setTimeout(getTeamData(), 1000);
-    console.log(cashBalance.get());
     return cashBalance.get();
   },
   s0001() {
-    console.log(s0001.get());
     return s0001.get();
   },
   s0005() {
-    console.log(s0005.get());
     return s0005.get();
   },
   s0386() {
-    console.log(s0386.get());
     return s0386.get();
   },
   s0388() {
-    console.log(s0388.get());
     return s0388.get();
   },
   s3988() {
-    console.log(s3988.get());
     return s3988.get();
-  }
+  },
+  getExchange() {
+    return getExchange.get();
+  },
+  getStock() {
+    return getStock.get();
+  },
+  getSide() {
+    return getSide.get();
+  },
+  getOrderType() {
+    return getOrderType.get();
+  },
+  getQuantity() {
+    return getQuantity.get();
+  },
+  getPrice() {
+    return getPrice.get();
+  },
 });
 
 var getTeamData=function(){
-  Meteor.call('getData','TtH8CwcTEcwcpP7BOoZBzg',function(err,res)
+  Meteor.call('getData','AT7pT3uUKObhrZN60C817Q',function(err,res)
 	{
 		if(!err){
       var jsonData = JSON.parse(res.content);
@@ -65,7 +83,16 @@ var getTeamData=function(){
         s3988.set(jsonData['3988'].split(".")[0]);
       } else { s3988.set(0)}
       
-      console.log(jsonData.cash.split(".")[0]);
+      // if (cashBalance.get() > jsonData.cash.split(".")[0]) {
+      //   var elem = document.getElementById("cashBalance");
+      //   elem.style.color = "Red";
+      // } else if (cashBalance.get() < jsonData.cash.split(".")[0]) {
+      //   var elem = document.getElementById("cashBalance");
+      //   elem.style.color = "Green";
+      // } else {
+      //   var elem = document.getElementById("cashBalance");
+      //   elem.style.color = "White";
+      // }
 
       if (cashBalance.get() != jsonData.cash.split(".")[0]){
         cashBalance.set(jsonData.cash.split(".")[0]);
@@ -79,3 +106,34 @@ var getTeamData=function(){
     }
 	});
 }
+
+Template.submit.events({
+  'click button'(event, instance) {
+    var form = document.forms['manualOrderForm'];
+    Meteor.call(
+      'manualOrder',
+      'AT7pT3uUKObhrZN60C817Q',
+      form.elements['exchange'].value,
+      form.elements['symbol'].value,
+      form.elements['side'].value,
+      form.elements['qty'].value,
+      form.elements['order_type'].value,
+      function(err,res){
+      if(!err){
+        var jsonData = JSON.parse(res.content);
+        getExchange.set(form.elements['exchange'].value)
+        getStock.set(jsonData.symbol)
+        getSide.set(jsonData.side)
+        getOrderType.set(jsonData.order_type)
+        getQuantity.set(jsonData.qty)
+        price = 0;
+        jsonData.fills.forEach(function(entry){
+          price = price + entry.price*entry.qty;
+        });
+        getPrice.set((price/getQuantity.get()).toFixed(5));
+      } else {
+        // console.log(err);
+      }
+    });
+  }
+});
--- packages/index/server/server.js
+++ packages/index/server/server.js
@@ -3,5 +3,18 @@ Meteor.methods({
         return HTTP.call("GET","https://cis2016-teamtracker.herokuapp.com/api/teams/"+teamID,{
 		headers:{"content-type":"application/json"}
 	});
+    },
+    manualOrder (teamID, exchange, symbol, side, qty, order_type) {
+    	console.log("manualOrder");
+    	return HTTP.call("POST","http://cis2016-"+exchange+".herokuapp.com/api/orders",{
+    		headers:{"content-type":"application/json"},
+    		data:{
+    			"team_uid": teamID,
+				"symbol": symbol,
+				"side": side,
+				"qty": qty,
+				"order_type": order_type
+    		}
+    	})
     }
 })
